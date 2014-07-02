33f88f9b8f983f932a9cc54ebeb41be84770621a
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-07-02 05:35:40 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TopicalBible::Application.config.secret_key_base = 'd6da31fa5d23fcc3a7e74ed15e16940e11ed6163ab218a42fed9c1e267a6b7eb3b63c511f8c273a73fa480774642134f2b9cced88d40172c4f6b8346c521a250'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TopicalBible::Application.config.session_store :cookie_store, key: '_topical-bible_session'
